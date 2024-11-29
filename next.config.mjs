058de17a411f7b47bab395cc65857f7b8b60fc7a
DESKTOP-8K60OKP\IS LAPTOP <syedsaim40@gmail.com>
2024-11-29 20:29:28 +0500

    build sucess

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: path.resolve(__dirname, 'build'), // Ensures absolute path
+    distDir: 'build',
     reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+
+export default nextConfig;

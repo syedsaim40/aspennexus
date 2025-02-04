a87a0d98437ca84612df724448d87db068de10d6
DESKTOP-8K60OKP\IS LAPTOP <syedsaim40@gmail.com>
2025-02-04 16:05:21 +0500

    code push

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import Header from '../src/app/Components/Header'
+import React from 'react';
+import Header from '../src/app/Components/Header';
+import './service.scss';
+import styles from "../src/app/page.module.css";
+import "../src/app/globals.css";
 
 
 function about() {

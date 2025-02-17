7a28df812a7ca585d87df9dd58dbebadc886a333
DESKTOP-8K60OKP\IS LAPTOP <syedsaim40@gmail.com>
2025-02-18 00:08:45 +0500

    worked on development service

--- services/development.js
+++ services/development.js
@@ -14,6 +14,16 @@ import faqImage from "../public/Images/faq.jpg";
 import serviceNav1 from "../public/Images/serviceNav2.svg";
 import Testimonials from '../src/app/Components/Testimonials';
 import FAQ from '../src/app/Components/FAQ';
+import dev1 from "../public/Images/development/dev1.png";
+import dev2 from "../public/Images/development/dev2.png";
+import dev3 from "../public/Images/development/dev3.png";
+import dev4 from "../public/Images/development/dev4.png";
+import dev5 from "../public/Images/development/dev5.png";
+import dev6 from "../public/Images/development/dev6.png";
+import dev7 from "../public/Images/development/dev7.png";
+import dev8 from "../public/Images/development/dev8.png";
+import dev9 from "../public/Images/development/dev9.png";
+import dev10 from "../public/Images/development/dev10.png";
 
 export default function development() {
   return (
@@ -36,7 +46,7 @@ export default function development() {
                   <a href={'/contact'} className='btn btn-over'>Book a free Consultation</a>
                 </div>
                 <div className="service_image">
-                  <Image src={faqImage} height={480} width={320} />
+                  <Image src={dev10} height={480} width={320} />
                 </div>
               </div>
               <div className="nav_heading">
@@ -49,70 +59,60 @@ export default function development() {
               <div className='service_Box'>
                 <div className='nav_Item'><h2>CMS Web Development</h2></div>
                 <div className='nav_Content'>
+                  <div className="nav_gallery">
+                      <div className="nav_img rounded-lg object-cover"><Image src={dev2} alt="alt" /></div>
+                      <div className="nav_img rounded-lg object-cover"><Image src={dev3} alt="alt" /></div>
+                  </div>
                   <p>Make content management easy with our custom-built CMS platforms designed to empower businesses online. From
 Joomla to WordPress, our CMS services offertailored solutions to meet your needs and streamline updates so you can
 have full control over your website.</p>
-                  <div className="nav_gallery">
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                  </div>
                 </div>
               </div>
               <div className='service_Box'>
                 <div className='nav_Item'><h2>B2C Web Development</h2></div>
                 <div className='nav_Content'>
+                  <div className="nav_gallery">
+                      <div className="nav_img rounded-lg object-cover"><Image src={dev4} alt="alt" /></div>
+                      <div className="nav_img rounded-lg object-cover"><Image src={dev5} alt="alt" /></div>
+                  </div>
                   <p>Offering visually appealing and engaging B2C websites to attract your customers. Our services caterto every kind of
 customer, helping you target your desired audience. We drive conversions by creating userfriendly interfaces.
 </p>
-                  <div className="nav_gallery">
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                  </div>
                 </div>
               </div>
               <div className='service_Box'>
                 <div className='nav_Item'><h2>PHP Web Development</h2></div>
                 <div className='nav_Content'>
+                  <div className="nav_gallery">
+                      <div className="nav_img rounded-lg object-cover"><Image src={dev6} alt="alt" /></div>
+                      <div className="nav_img rounded-lg object-cover"><Image src={dev7} alt="alt" /></div>
+                  </div>
                   <p>We offerrobust and feature rich web solutions by leveraging PHP frameworks like Laravel and CodeIgniter, providing you
                   seamless functionality from custom applications to dynamic websites</p>
-                  <div className="nav_gallery">
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                  </div>
                 </div>
               </div>
               <div className='service_Box'>
                 <div className='nav_Item'><h2>WordPress Web Development</h2></div>
                 <div className='nav_Content'>
+                  <div className="nav_gallery">
+                      <div className="nav_img rounded-lg object-cover"><Image src={dev8} alt="alt" /></div>
+                      <div className="nav_img rounded-lg object-cover"><Image src={dev9} alt="alt" /></div>
+                  </div>
                   <p>From a simple blog to a complex ecommerce platform, we unlock the potential of your website with our WordPress web
 development services. Our experts help you build a site thatis both secured and fully optimized for search engines.
 </p>
-                  <div className="nav_gallery">
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                  </div>
                 </div>
               </div>
               <div className='service_Box'>
                 <div className='nav_Item'><h2>B2B Portal Development</h2></div>
                 <div className='nav_Content'>
+                  <div className="nav_gallery">
+                      <div className="nav_img rounded-lg object-cover"><Image src={dev1} alt="alt" /></div>
+                      <div className="nav_img rounded-lg object-cover"><Image src={dev10} alt="alt" /></div>
+                  </div>
                   <p>We transform your business with a customized B2B portal. We promote efficiency and fosterlong term partnerships by
 developing platforms that streamline interactions. Whetheritis order management system or product catalogues, we
 provide tailored portals meeting your needs.</p>
-                  <div className="nav_gallery">
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                      <div className="nav_img rounded-lg object-cover"><Image src={serviceNav1} alt="alt" /></div>
-                  </div>
                 </div>
               </div>
             </div>
